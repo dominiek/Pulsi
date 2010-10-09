8ed2683f30e242a33a7d614013394e1b0f13056c
Dominiek ter Heide <dominiek@cerego.com>
2010-10-09 14:07:16 +0200

    Added some mock objects.

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -2,3 +2,11 @@
 var User = function() {
   
 };
+
+User.prototype.portfolio = function(username, callback) {
+  callback([
+    {company_identifier: 'dropbox', num_shares: 10}
+  ]);
+};
+
+var user = new User();
